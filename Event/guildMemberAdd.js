eb32bee0b25f8a6db580eabf7973bbfdd94a6d6e
AzerX2 <bpt.cor@gmail.com>
2023-09-29 22:12:21 +0200

    {guildMemberAdd}
    Ajout bienvenue et ajout de rôle à l'arrivé

--- Event/guildMemberAdd.js
+++ Event/guildMemberAdd.js
@@ -7,6 +7,15 @@
  * @description Cette fonction est appelée lorsque un membre rejoint un serveur.
  */
 module.exports = async(client, guildMember) => {
-    const channel = client.channels.cache.get("ID_CHANNEL");
-    channel.send(`Bienvenue ${guildMember} sur le serveur ${guildMember.guild.name} !`);
+
+    const channel = client.channels.cache.get("1150764608353751082");
+    channel.send(`Bienvenue ${guildMember} sur le serveur du groupe 2 !`);
+
+    // on lui ajout le rôle : 1150764750641299556 
+    let role = guildMember.guild.roles.cache.get("1150764750641299556")
+    try {
+        guildMember.roles.add(role)
+    } catch (error) {
+        console.log(error)
+    }
 };
