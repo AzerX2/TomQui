cebaedea2ee8c6d40cb1ded2584e090a8c83a677
AzerX2 <bpt.cor@gmail.com>
2023-09-29 20:42:40 +0200

    {create-session} Ajout commande

---
+++ SlashCommandes/create-session.js
@@ -0,0 +1,50 @@
+/**
+ * @file create-session.js
+ * @description Ce fichier sert à créer une session de travail.
+ */
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('create-session')
+        .setDescription('permet de créé une session de travail')
+        .addStringOption(option => option.setName('nom').setDescription('nom de la session (mettre matière)').setRequired(true))
+        .addStringOption(option => option.setName('date').setDescription('date de la session forme : mois-jour-heure exemple : 9-10-12 : 10 septembre à 12h ').setRequired(true))
+        .addBooleanOption(option => option.setName('recurrent').setDescription('si la session est recurrente').setRequired(true)),
+    async execute(interaction) {
+        // si l'utilisateur n'a pas les permissions admin il ne peut pas utiliser la commande
+        if (!interaction.member.permissions.has('ADMINISTRATOR')) return interaction.reply({ content: "Vous n'avez pas les permissions pour utiliser cette commande", ephemeral: true });
+
+        let session = require('../models/session.js');
+
+        let nom = interaction.options.getString('nom');
+        let date = interaction.options.getString('date');
+        let recurrent = interaction.options.getBoolean('recurrent');
+
+        let year = "2023"
+        let dateFormated = date.split("-")
+        let month = dateFormated[0]
+        let day = dateFormated[1]
+        let hour = dateFormated[2]
+
+        // pour month on va enlever 1 car le mois commence à 0 et non à 1
+        let dateFinal = new Date(year, month - 1, day, hour)
+
+        let sessionToCreate = new session({
+            nom: nom,
+            date: dateFinal,
+            recurrent: recurrent,
+            userlist: []
+        })
+        sessionToCreate.save().then(() => {
+            interaction.reply({ content: "Session créé avec succès" })
+        }).catch((err) => {
+            console.log(err)
+            interaction.reply({ content: "Une erreur est survenue lors de la création de la session" })
+        })
+
+
+
+    }
+};
--- models/session.js
+++ models/session.js
@@ -2,8 +2,9 @@ const mongo = require('mongoose')
 
 const Schema = new mongo.Schema({
     id: String,
+    nom: String,
     userlist: [String],
-    date: String,
+    date: Date,
     recurrent: Boolean
 });
 
